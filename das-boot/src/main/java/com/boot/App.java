a8285e65a55920db6196d835ef123b6e70fb185f
swathicheela <swathi cheela@LAPTOP-SBSTG7U1>
2017-12-02 11:19:55 -0500

    created a simple spring boot app

---
+++ das-boot/src/main/java/com/boot/App.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author swathi cheela
+ *
+ */
+@SpringBootApplication
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+
+	}
+
+}
